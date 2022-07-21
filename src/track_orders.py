fb2fd7c0fd8459f01df54efb7808294428d525fd
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-07-21 17:06:19 -0300

    feat(TrackOrders): implements method add_new_order

--- src/track_orders.py
+++ src/track_orders.py
@@ -7,7 +7,7 @@ class TrackOrders:
         return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
         pass
